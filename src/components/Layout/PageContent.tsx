61d9aa636869f32d7d4e521e27cff1573cfb5803
Elham-EN <ilhamrezaie@gmail.com>
2023-07-02 14:01:00 +1000

    made Page content responsive for all screen sizes

--- src/components/Layout/PageContent.tsx
+++ src/components/Layout/PageContent.tsx
@@ -9,10 +9,31 @@ interface PageContentProps {
 // Easy way to create Custom Page Layout
 function PageContent({ children }: PageContentProps): ReactElement {
   return (
-    <Flex>
-      <Flex>
-        <Flex>{children && children[0 as keyof typeof children]}</Flex>
-        <Flex>{children && children[1 as keyof typeof children]}</Flex>
+    <Flex justify={"center"} padding={"16px 0px"} border={"5px solid red"}>
+      <Flex
+        width={"90%"}
+        justify={"center"}
+        maxWidth={"1460px"}
+        border={"5px solid green"}
+      >
+        {/* LHS */}
+        <Flex
+          direction={"column"}
+          width={{ base: "100%", md: "65%" }}
+          mr={{ base: 0, md: 6 }}
+          border={"5px solid blue"}
+        >
+          {children && children[0 as keyof typeof children]}
+        </Flex>
+        {/* RHS */}
+        <Flex
+          direction={"column"}
+          display={{ base: "none", md: "flex" }}
+          flexGrow={1}
+          border={"5px solid orange"}
+        >
+          {children && children[1 as keyof typeof children]}
+        </Flex>
       </Flex>
     </Flex>
   );
