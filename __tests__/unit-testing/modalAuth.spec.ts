ecd34d2edeb1b6c239a65bba225d53db23fca62e
Elham-EN <ilhamrezaie@gmail.com>
2023-07-11 00:20:50 +1000

    unit test to test if auth modal show when button is clicked

---
+++ __tests__/unit-testing/modalAuth.spec.ts
@@ -0,0 +1,25 @@
+import { test, expect } from "@playwright/test";
+
+test("Should open login Modal", async ({ page }) => {
+  // start from the index page (the baseURL)
+  await page.goto("http://localhost:3000/");
+  // find an button element with text 'Log in' and click on it
+  await page.click("#login-button");
+  // Expect the modal to be open
+  const modal = await page.waitForSelector(".chakra-modal__content", {
+    state: "visible",
+  });
+  expect(modal).toBeTruthy();
+});
+
+test("Should open signup Modal", async ({ page }) => {
+  // start from the index page (the baseURL)
+  await page.goto("http://localhost:3000/");
+  // find an button element with text 'signup' and click on it
+  await page.click("#signup-button");
+  // Expect the modal to be open
+  const modal = await page.waitForSelector(".chakra-modal__content", {
+    state: "visible",
+  });
+  expect(modal).toBeTruthy();
+});
