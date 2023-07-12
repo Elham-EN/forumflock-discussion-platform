f4bb046e8e32683017a753c2f886931b8ee65ef7
Elham-EN <ilhamrezaie@gmail.com>
2023-07-12 21:06:26 +1000

    Setup Action CI and workflow to test the web app on every push

---
+++ src/components/Post/NewPostForm.tsx
@@ -0,0 +1,7 @@
+import React, { ReactElement } from "react";
+
+type Props = {};
+
+export default function NewPostForm({}: Props): ReactElement {
+  return <div>NewPostForm</div>;
+}
--- src/pages/f/[communityId]/submit.tsx
+++ src/pages/f/[communityId]/submit.tsx
@@ -1,5 +1,33 @@
+import PageContent from "@/components/Layout/PageContent";
+import NewPostForm from "@/components/Post/NewPostForm";
+import { Box, Divider, Icon, Text } from "@chakra-ui/react";
 import React, { ReactElement } from "react";
+import { MdCreate } from "react-icons/md";
 
 export default function Submit(): ReactElement {
-  return <div>Submit Page</div>;
+  return (
+    <PageContent>
+      <>
+        <Box
+          display={"flex"}
+          alignItems={"center"}
+          //   justifyContent={"center"}
+          fontSize={"24px"}
+          gap={2}
+          p={5}
+          borderBottom={"1px solid"}
+          borderBottomColor={"white"}
+        >
+          <Icon as={MdCreate} />
+          <Text fontSize={"18pt"} fontWeight={700} fontFamily={"sans-serif"}>
+            Create a post
+          </Text>
+        </Box>
+        <NewPostForm />
+      </>
+      <>
+        <div>About community</div>
+      </>
+    </PageContent>
+  );
 }
