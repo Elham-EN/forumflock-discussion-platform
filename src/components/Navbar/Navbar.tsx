44a4f318b59576d58f7751e6e173d7207ba79153
Elham-EN <ilhamrezaie@gmail.com>
2023-08-07 15:13:23 +1000

    fixed logo

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,10 +1,12 @@
 import React from "react";
-import { Flex, Image } from "@chakra-ui/react";
+import { Flex, Icon, Image, Text } from "@chakra-ui/react";
 import SearchInput from "./SearchInput";
 import RightContent from "./RightContent/RightContent";
 import Directory from "./Directory/Directory";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "@/firebase/clientApp";
+import { BiFace } from "react-icons/bi";
+import Link from "next/link";
 
 export default function Navbar(): React.ReactElement {
   const [user, loading, error] = useAuthState(auth);
@@ -17,21 +19,14 @@ export default function Navbar(): React.ReactElement {
       width={"100%"}
       zIndex={500}
     >
-      <Flex align={"center"}>
-        <Image
-          src="/images/fedditLogo1.png"
-          alt="Feddit Logo"
-          height={"50px"}
-          minW={"150px"}
-          display={{ base: "none", md: "unset" }}
-        />
-        <Image
-          src="/images/fedditLogoNoText1.png"
-          alt="Feddit Logo"
-          height={"40px"}
-          minW={"45px"}
-          display={{ md: "none" }}
-        />
+      <Flex align={"center"} mt={2}>
+        <Link href={"/"}>
+          <Icon
+            color={"brand.100"}
+            as={BiFace}
+            boxSize={{ base: 10, sm: "16" }}
+          />
+        </Link>
       </Flex>
       <Directory />
       <SearchInput />
