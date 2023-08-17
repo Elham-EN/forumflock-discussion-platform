921feb4ca974d749db13f771c29531332f0ab19f
Elham-EN <ilhamrezaie@gmail.com>
2023-08-17 13:37:28 +1000

    created data model and recoil state for Directory Menu

---
+++ src/atoms/directoryMenuAtom.ts
@@ -0,0 +1,38 @@
+import { IconType } from "react-icons";
+import { TiHome } from "react-icons/ti";
+import { atom } from "recoil";
+
+// Represent the Item in the Directory Menu
+export interface DirectoryMenuItem {
+  displayText: string;
+  link: string;
+  icon: IconType;
+  iconColor: string;
+  imageURL?: string;
+}
+
+// Directory Menu State
+interface DirectoryMenuState {
+  isOpen: boolean;
+  selectedMenuItem: DirectoryMenuItem;
+}
+
+// By default the homepage, when not in community page
+export const defaultMenuItem: DirectoryMenuItem = {
+  displayText: "Home",
+  link: "/",
+  icon: TiHome,
+  iconColor: "black",
+};
+
+// Default state for Directory Menu
+export const defaultMenuState: DirectoryMenuState = {
+  isOpen: false,
+  selectedMenuItem: defaultMenuItem,
+};
+
+// Atom global piece of state: Directory Menu State
+export const directroyMenuState = atom<DirectoryMenuState>({
+  key: "directoryMenuState",
+  default: defaultMenuState,
+});
