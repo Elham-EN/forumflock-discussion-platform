ac7c0ae05a1d0babb724c1c9e6eb4b2c84c5a401
Elham-EN <ilhamrezaie@gmail.com>
2023-09-20 19:06:16 +1000

    implemented the infinite scrolling for non-auth user homefeeds

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,20 +2,24 @@
 // ensures that the component follows the expected structure and
 // has the correct props according to the NextPage
 import type { NextPage } from "next";
-
 import PageContent from "@/components/Layout/PageContent";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth, firestore } from "@/firebase/clientApp";
 import { useEffect, useState } from "react";
 import { Flex, Stack, Text } from "@chakra-ui/react";
 import {
+  DocumentData,
+  FirestoreError,
+  Query,
   collection,
   getDocs,
   limit,
   orderBy,
   query,
+  startAfter,
   where,
 } from "firebase/firestore";
+import InfiniteScroll from "react-infinite-scroll-component";
 import UsePosts from "@/hooks/usePosts";
 import { Post, PostVote } from "@/atoms/postsAtom";
 import PostLoader from "@/components/Post/PostLoader";
@@ -37,6 +41,11 @@ const Home: NextPage = () => {
     onSelectPost,
     onVote,
   } = UsePosts();
+
+  // For Pagination
+  const [hasMore, setHasMore] = useState<boolean>(true);
+  const [lastVisible, setLastVisible] = useState<null | DocumentData>(null);
+  const [posts, setPosts] = useState<Post[]>([]);
 
   // Build feed for authenticated users
   const buildUserHomeFeed = async () => {
@@ -94,6 +103,51 @@ const Home: NextPage = () => {
     setLoading(false);
   };
 
+  const fetchPostsForNonAuthUsers = async (last?: DocumentData | null) => {
+    setLoading(true);
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    let postQuery: Query<DocumentData>;
+    if (last) {
+      postQuery = query(
+        collection(firestore, "posts"),
+        orderBy("voteStatus", "desc"),
+        startAfter(last),
+        limit(5)
+      );
+    } else {
+      postQuery = query(
+        collection(firestore, "posts"),
+        orderBy("voteStatus", "desc"),
+        limit(5)
+      );
+    }
+    try {
+      const postDocs = await getDocs(postQuery);
+      const newPosts = postDocs.docs.map(
+        (doc) =>
+          ({
+            id: doc.id,
+            ...doc.data(),
+          } as Post)
+      );
+
+      if (newPosts.length > 0) {
+        setLastVisible(postDocs.docs[postDocs.docs.length - 1]);
+        setPostStateValue((prev) => ({
+          ...prev,
+          posts: [...prev.posts, ...newPosts],
+        }));
+        setPosts((prevPosts) => [...prevPosts, ...newPosts]);
+      } else {
+        setHasMore(false);
+      }
+    } catch (error) {
+      const firestoreError = error as FirestoreError;
+      console.log("Fetch Post Error: ", firestoreError.message);
+    }
+    setLoading(false);
+  };
+
   const getUserPostVotes = async () => {
     try {
       const postIds = postStateValue.posts.map((post) => post.id);
@@ -130,7 +184,8 @@ const Home: NextPage = () => {
   useEffect(() => {
     // if there is no user and not longer attempting to
     // fetch the user, then buildNoUserHomeFeed
-    if (!user && !loadingUser) buildNoUserHomeFeed();
+    if (!user && !loadingUser && postStateValue.posts.length === 0)
+      fetchPostsForNonAuthUsers();
   }, [user, loadingUser]);
 
   // We call this function until, it has fetched community
@@ -142,7 +197,7 @@ const Home: NextPage = () => {
   return (
     <PageContent>
       <>
-        <CreatePostLink />
+        {/* <CreatePostLink />
         {loading ? (
           <PostLoader />
         ) : (
@@ -164,7 +219,38 @@ const Home: NextPage = () => {
               />
             ))}
           </Stack>
-        )}
+        )} */}
+        <CreatePostLink />
+        <InfiniteScroll
+          dataLength={postStateValue.posts.length}
+          next={() => fetchPostsForNonAuthUsers(lastVisible)}
+          hasMore={hasMore}
+          loader={<PostLoader />} // Replace with your loading component or spinner
+          endMessage={
+            <p style={{ textAlign: "center" }}>
+              <b>Yay! You have seen it all</b>
+            </p>
+          }
+        >
+          <Stack spacing={3}>
+            {postStateValue.posts.map((post) => (
+              <PostItem
+                key={post.id}
+                post={post}
+                onDeletePost={onDeletePost}
+                onSelectPost={onSelectPost}
+                onVote={onVote}
+                userVoteValue={
+                  postStateValue.postVotes.find(
+                    (item) => item.postId === post.id
+                  )?.voteValue
+                }
+                userIsCreator={user?.uid === post.creatorId}
+                homePage={true}
+              />
+            ))}
+          </Stack>
+        </InfiniteScroll>
       </>
       <>
         <Recommendation />
